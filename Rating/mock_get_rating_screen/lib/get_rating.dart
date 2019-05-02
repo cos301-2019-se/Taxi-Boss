830c2b3a01e2a861c005d7f415e2ef8b4472d8f8
Waldo <u15018556@tuks.co.za>
2019-05-02 13:01:34 +0200

    Added dependencies for testing mock app

--- Rating/mock_get_rating_screen/lib/get_rating.dart
+++ Rating/mock_get_rating_screen/lib/get_rating.dart
@@ -1,7 +1,16 @@
 import 'package:firebase_core/firebase_core.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class GetAvgRating {
   
+  int comfort = 0, safety = 0, etiquette = 0;
+
+  void collectData(){
+    //connect and get data from database here
+    String id;
+    final db = Firestore.instance;
+  }
+
   int getAvgRatingComfort()
   {
     return 0;
