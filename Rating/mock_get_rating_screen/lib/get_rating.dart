4d51afbe8c7b72e6510bad9be913029266baa62f
Waldo <u15018556@tuks.co.za>
2019-05-05 16:47:28 +0200

    Added unit testing for driver app

--- Rating/mock_get_rating_screen/lib/get_rating.dart
+++ Rating/mock_get_rating_screen/lib/get_rating.dart
@@ -32,12 +32,16 @@ class GetAvgRating {
           avgRating += snap.documents.elementAt(i).data['averageRating'];
         }
       }
-      comfort = comfort/count;
-      safety = safety/count;
-      etiquette = etiquette/count;
-      avgRating = avgRating/count;
+      comfort = calcAvg(comfort,count);
+      safety = calcAvg(safety,count);
+      etiquette = calcAvg(etiquette,count);
+      avgRating = calcAvg(avgRating,count);
       done = true;
     });    
+  }
+
+  double calcAvg(double sum, int count){
+    return sum/count;
   }
 
   double getAvgRatingComfort()
