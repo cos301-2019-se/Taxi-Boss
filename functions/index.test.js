3264705b6c2bdfbcc893e766c19917421b9979d7
TransvaalseSeePloeg <47810853+u15030378@users.noreply.github.com>
2019-07-18 23:41:16 +0200

    Removed public api key

--- functions/index.test.js
+++ functions/index.test.js
@@ -1,18 +1,13 @@
 
 
-const test = require('firebase-functions-test')({
-    databaseURL: 'https://taxi-boss.firebaseio.com',
-    storageBucket: 'taxi-boss.appspot.com',
-    projectId: 'taxi-boss',
-  }, 'taxi-boss-5e70baeac9b9.json');
+
 
   const mocha=require('mocha');
   const chai=require('chai');
 
   const functions= require('firebase-funcitons');
-  const key = functions.config().stripe.key;
 
-  test.mockConfig({stripe: {key: 'AIzaSyDcPdaDs-ixSojbM4P_DhOO4-x05QG0_08'}});
+
 
   const api= require('../functions/index')
 
