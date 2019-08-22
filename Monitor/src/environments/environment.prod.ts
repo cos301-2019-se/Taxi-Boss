1a2525ffa28e7cfc4e230145f4cab79849879d67
brian-ndungu <u15322913@tuks.co.za>
2019-08-23 00:02:41 +0200

    Security Change

--- Monitor/src/environments/environment.prod.ts
+++ Monitor/src/environments/environment.prod.ts
@@ -6,12 +6,6 @@
 export const environment = {
   production: true,
   firebaseConfig : {
-    apiKey: "AIzaSyC1FK6R2NLk0cfhMsMoE4EJnsNZaB47hfk",
-    authDomain: "taxi-boss-3792e.firebaseapp.com",
-    databaseURL: "https://taxi-boss-3792e.firebaseio.com",
-    projectId: "taxi-boss-3792e",
-    storageBucket: "taxi-boss-3792e.appspot.com",
-    messagingSenderId: "778634859060",
-    appId: "1:778634859060:web:2f2cd461b243ee10"
+    
   }
 };
